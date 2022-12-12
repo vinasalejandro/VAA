5177e4f998c208b594bff99c06ff470e122d00c3
vinasalvarez <vinasalejandro81@gmail.com>
2022-12-12 21:36:36 +0100

    Scripts PowerShell

---
+++ Scripts_PowerShell/3_Creacion_BD.ps1
@@ -0,0 +1,82 @@
+﻿
+Get-InstalledModule -Name "SqlServer"
+
+Install-Module -Name "SqlServer"
+
+
+#Creamos directorios
+New-Item "C:\Scripts" -itemType Directory
+
+New-Item "C:\BD" -itemType Directory
+
+
+#Creamos la BD desde el script de la ruta
+
+Invoke-SqlCmd -ServerInstance localhost -InputFile C:\scripts\Create_MyDatabase.sql
+
+#Lanzamos una Query para borrar la BD
+
+Invoke-SqlCmd -ServerInstance localhost -Query "DROP DATABASE MyDatabase"
+
+######################################################################################################################################
+
+#create variable with SQL to execute
+
+#creamos la variable con el codigo sql
+$sql = "
+CREATE DATABASE [MyDatabase]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'MyDatabase', FILENAME = N'C:\BD\MyDatabase.mdf' , SIZE = 1048576KB , FILEGROWTH = 262144KB )
+ LOG ON 
+( NAME = N'MyDatabase_log', FILENAME = N'C:\BD\MyDatabase_log.ldf' , SIZE = 524288KB , FILEGROWTH = 131072KB )
+GO
+ 
+USE [master]
+GO
+ALTER DATABASE [MyDatabase] SET RECOVERY SIMPLE WITH NO_WAIT
+GO
+ 
+ALTER AUTHORIZATION ON DATABASE::[MyDatabase] TO [sa]
+GO"
+
+Invoke-SqlCmd -ServerInstance localhost -Query $sql
+
+
+#########################################################################################################################
+
+
+# Method # 3 - Create SQL Server Database Using PowerShell and dbatools
+
+# verify you have dbatools module installed
+Get-InstalledModule -Name "dbatools"	
+
+Install-Module -Name "dbatools"
+
+
+###Cremos un BD
+#creamos las variables y ejecutamos la creación de la BD con las variables
+
+$SqlInstance = 'localhost'                                                          # SQL Server name 
+$Name = 'MyDatabaseDDATOOLS'                                                        # database name
+$DataFilePath = 'C:\BD\'                                                            # data file path
+$LogFilePath = 'C:\BD\'                                                             # log file path
+$Recoverymodel = 'Simple'                                                           # recovery model
+$Owner = 'sa'                                                                       # database owner
+$PrimaryFilesize = 1024                                                             # data file initial size
+$PrimaryFileGrowth = 256                                                            # data file autrogrowth amount
+$LogSize = 512                                                                      # data file initial size
+$LogGrowth = 128                                                                    # data file autrogrowth amount
+ 
+New-DbaDatabase -SqlInstance $SqlInstance -Name $Name -DataFilePath $DataFilePath -LogFilePath $LogFilePath -Recoverymodel $Recoverymodel -Owner $Owner -PrimaryFilesize $PrimaryFilesize -PrimaryFileGrowth $PrimaryFileGrowth -LogSize $LogSize -LogGrowth $LogGrowth | Out-Null
+
+
+#Comprobamos que se creó la BD
+
+Get-DbaDatabase -SqlInstance $SqlInstance -Database $Name
+
+#Eliminamos la BD
+
+Invoke-SqlCmd -ServerInstance localhost -Query "DROP DATABASE MyDatabaseDDATOOLS"
+
+
---
+++ Scripts_PowerShell/Ejemplo-Primero_ADO_Net.ps1
@@ -0,0 +1,83 @@
+﻿
+#Listing 1: Retrieving Data Through a SqlDataReader Object
+
+#Listado de datos con string
+
+#Ejemplo con AdventureWorks2019
+
+# Create SqlConnection object and define connection string
+$con = New-Object System.Data.SqlClient.SqlConnection
+$con.ConnectionString = "Server=.; Database=AdventureWorks2019;   Integrated Security=true"
+$con.open()
+
+# Create SqlCommand object, define command text, and set the connection
+$cmd = New-Object System.Data.SqlClient.SqlCommand
+$cmd.CommandText = "SELECT TOP 10 FirstName, LastName FROM Person.Person"
+$cmd.Connection = $con
+
+# Create SqlDataReader
+$dr = $cmd.ExecuteReader()
+
+Write-Host
+
+If ($dr.HasRows)
+{
+  Write-Host Number of fields: $dr.FieldCount
+  Write-Host
+  While ($dr.Read())
+  {
+    Write-Host $dr["FirstName"] $dr["LastName"]
+  }
+}
+Else
+{
+  Write-Host The DataReader contains no rows.
+}
+
+Write-Host
+
+# Close the data reader and the connection
+$dr.Close()
+$con.Close()
+
+
+
+########################################################################################################################################
+
+#Ejemplo con pubs (tabla authors, muestra el id y el nombre)
+
+# Create SqlConnection object and define connection string
+$con = New-Object System.Data.SqlClient.SqlConnection
+$con.ConnectionString = "Server=.; Database=pubs;   Integrated Security=true"
+$con.open()
+
+# Create SqlCommand object, define command text, and set the connection
+$cmd = New-Object System.Data.SqlClient.SqlCommand
+$cmd.CommandText = "SELECT TOP 10 au_id, au_lname FROM dbo.authors"
+$cmd.Connection = $con
+
+# Create SqlDataReader
+$dr = $cmd.ExecuteReader()
+
+Write-Host
+
+If ($dr.HasRows)
+{
+  Write-Host Number of fields: $dr.FieldCount
+  Write-Host
+  While ($dr.Read())
+  {
+    Write-Host $dr["au_id"] $dr["au_lname"]
+  }
+}
+Else
+{
+  Write-Host The DataReader contains no rows.
+}
+
+Write-Host
+
+# Close the data reader and the connection
+$dr.Close()
+$con.Close()
+
