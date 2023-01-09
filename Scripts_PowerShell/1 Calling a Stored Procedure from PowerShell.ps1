3e0e9c5c0c6d84b78a1976d15f3ebb8a3f649b28
vinasalvarez <vinasalejandro81@gmail.com>
2023-01-09 21:34:11 +0100

    Scripts PowerShell

--- Scripts_PowerShell/1 Calling a Stored Procedure from PowerShell.ps1
+++ Scripts_PowerShell/1 Calling a Stored Procedure from PowerShell.ps1
@@ -2,7 +2,7 @@
 
 #Usamos para crear el procedimieto el .sql con el mismo nombre
 
-Invoke-Sqlcmd -ServerInstance localhost -Database TEMPDB -Query "CustomerSales"
+Invoke-Sqlcmd -ServerInstance localhost -Database TEMPDB -Query "CustomerSales" ##Ejecuta el procedimiento almacenado "CustomerSales"
 
 
 
@@ -24,41 +24,53 @@ notepad c:\sales.csv
 
 ###############
 
+
 # Calling Procs from PowerShell with Parameters
+
+#Ejecutar procedimientos almacenados con parámetros de entrada
 
 $results = Invoke-Sqlcmd -ServerInstance localhost -Database AdventureWorks2019 -Query "uspGetBillOfMaterials" #no funciona ya que faltan los parámetros de entrada
 
+#Añadimos parámetros de entrada
 $results = Invoke-Sqlcmd -ServerInstance localhost -Database AdventureWorks2019 -Query "[dbo].[uspGetBillOfMaterials] @StartProductID = 749, @CheckDate = '2010-05-26'"
 $results 
 
-$productid = 749
+#Añadimos los parámetros de entrada como variables
+$productid = "749"
 $checkdate = "2010-05-26"
 $results = Invoke-Sqlcmd -ServerInstance localhost -Database AdventureWorks2019 -Query "[dbo].[uspGetBillOfMaterials] @StartProductID = $productid, @CheckDate = '$checkdate'"
 $results | Export-Csv -Path "c:\sproc.csv" -NoTypeInformation
-notepad sproc.csv
+notepad c:\sproc.csv
 
 
-############################3
+#############################################################################################################################################################
 
 # ADO.NET
 
 $Server = "localhost"
 $Database = "AdventureWorks2019"
-$SqlConn = New-Object System.Data.SqlClient.SqlConnection("Server = $Server; Database = $Database; Integrated Security = True;")
-$SqlConn.Open()
-$cmd = $SqlConn.CreateCommand()
+$SqlConn = New-Object System.Data.SqlClient.SqlConnection("Server = $Server; Database = $Database; Integrated Security = True;") #Se usa la libreria de clases de ADO.net y se añade la cadena de conexión
+$SqlConn.Open() #abrimos la conexión
+$cmd = $SqlConn.CreateCommand() #creamos el objeto
+
+#Llamamos el procedimiento almacenado
 $cmd.CommandType = 'StoredProcedure'
 $cmd.CommandText = 'dbo.uspGetBillOfMaterials'
+
+#Añadimos parámetros de entrada
 $p1 = $cmd.Parameters.Add('@StartProductID',[int])
 $p1.ParameterDirection.Input
 $p1.Value = 749
 $p2 = $cmd.Parameters.Add('@CheckDate',[DateTime])
 $p2.ParameterDirection.Input
 $p2.Value = '2010-05-26'
+
 $results = $cmd.ExecuteReader()
 $dt = New-Object System.Data.DataTable
 $dt.Load($results)
-$SqlConn.Close()
+
+$SqlConn.Close() #cerramos la conexión
+
 $dt | Export-Csv -LiteralPath "C:\sproc.txt" -NoTypeInformation
 notepad C:\sproc.txt
 
