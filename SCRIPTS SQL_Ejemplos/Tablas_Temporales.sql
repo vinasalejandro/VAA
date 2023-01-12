3f8e6673086e3853f296e97b7441874e918c12e7
vinasalvarez <vinasalejandro81@gmail.com>
2023-01-12 18:49:42 +0100

    Scripts SQL tablas temporales

--- SCRIPTS SQL_Ejemplos/Tablas_Temporales.sql
+++ SCRIPTS SQL_Ejemplos/Tablas_Temporales.sql
@@ -1,3 +1,21 @@
+
+--TABLAS TEMPORALES
+
+--Ejemplo colegio médico
+
 USE master
 GO
+
+DROP DATABASE IF EXISTS Colegio_Medico
+GO
+
+CREATE DATABASE Colegio_Medico
+	ON PRIMARY (NAME = 'Colegio_Medico',
+	FILENAME = 'C:\Data\Colegio_Medico_Fijo.mdf',
+	SIZE = 15360kb, MAXSIZE = UNLIMITED, FILEGROWTH = 0)
+	LOG ON ( NAME = 'Colegio_Medico_log',
+	FILENAME = 'C:\Data\Colegio_Medico_log.ldf',
+	SIZE = 10176KB, MAXSIZE = 2048GB, FILEGROWTH = 10%)
+GO
+
 
